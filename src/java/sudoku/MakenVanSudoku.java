76c572cbef5aa770a873d5203cd5e3e57d28036f
SaddiquiShan <shan.saddiqui@student.howest.be>
2017-03-28 11:34:13 +0200

    Het toevoegen van random cijfers in een 2D array

--- src/java/sudoku/MakenVanSudoku.java
+++ src/java/sudoku/MakenVanSudoku.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sudoku;
 
 /**
@@ -11,13 +6,16 @@ package sudoku;
  */
 public class MakenVanSudoku {
     public static void main(String[]args){
-    Scanner input = new Scanner(System.in);
     int multTableSize = 9;
-    int[][] multTable = new int[multTableSize][multTableSize];
+    int[][] multTable = new int[multTableSize][multTableSize]; // 2 arrays
     
-    for (int row = 0; row < multTable.length; row++) {
-        for (int column = 0; column < multTable[row].length; column++) {
-            multTable[row][column] = (row + 1) * (column + 1);
+    for (int row = 0; row < multTable.length; row++) { // Zolang row kleiner is dan de lengte van multTable (wat 9 is). row++
+        for (int column = 0; column < multTable[row].length; column++) { // Zolang column kleiner is dan multTable[row] (wat telkens ook optelt met het vorige). column ++
+            
+            multTable[row][column] =(int)(Math.random()*9)+1; //
+            
+            //multTable[column] = (int)(Math.random()*multTableSize)+1;
+            //multTable[row][column] = (row + 1) * (column + 1); // Getallen in 
             System.out.printf("%3d", multTable[row][column]);
         }
         System.out.println();
